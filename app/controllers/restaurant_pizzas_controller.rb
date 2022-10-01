67436f1f2198b8f2f51e96f14b7585df6db82229
Mutaga Kihahu <kihahumutagah@gmail.com>
2022-10-01 13:52:41 +0300

    Added the create action in the RestaurantPizza controller

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,2 +1,21 @@
 class RestaurantPizzasController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :invalid_record
+    def index
+        restaurant_pizza = RestaurantPizza.all
+        render json: restaurant_pizza, status: :ok
+    end
+    def create
+        restaurant_pizza = RestaurantPizza.create!(restaurant_params)
+        render json: restaurant_pizza,status: :created
+    end
+
+    private
+
+    def restaurant_params
+        params.permit(:restaurant_id, :pizza_id, :price)
+    end
+
+    def invalid_record
+        render json: {errors: "validation errors"}, status: :unprocessable_entity
+    end
 end
