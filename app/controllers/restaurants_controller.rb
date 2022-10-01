44f722e0192f678b7ad72559f2189d9b4227dd11
Mutaga Kihahu <kihahumutagah@gmail.com>
2022-10-01 11:36:03 +0300

    Added the show action in restaurant that includes pizzas from a certain restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,17 @@
 class RestaurantsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :unfound_response
     def index
         restaurants = Restaurant.all
         render json: restaurants, status: :ok
+    end
+    
+    def show
+        restaurant = Restaurant.find(params[:id])
+        render json: restaurant,include: :pizzas
+    end
+
+    private
+    def unfound_response
+        render json: {error: "Restaurant not found"},status: :not_found
     end
 end
